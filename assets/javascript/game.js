9df708cbadde0731b95e206e882ba0b57242d89f
Alex H <08ahayden@gmail.com>
2017-12-06 21:25:21 -0500

    Put everything into functions, working on making logic work

---
+++ assets/javascript/game.js
@@ -0,0 +1,199 @@
+
+
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var secretwords = ["Gandalf", "Valinor", "Sauron", "Galadriel", "Balrog", "Gondor", "Rohan", "Moria", "Shire", "Baggins", "Gollum", "Frodo", "Orc", "Troll", "Dwarf", "Fellowship", "Aragorn", "Boromir", "Legolas",
+"Merry", "Pippin", "Sam"];
+
+var word;
+var userguess;
+var answerarray = [];
+var remainingguesses;
+var startvar;
+var wins;
+var losses;
+var spaces;
+var picture;
+
+
+function chooseword (){
+  word = secretwords[Math.floor(Math.random() * secretwords.length)].toLowerCase();
+  word = word.split("");
+
+};
+
+function returnblanks (){
+for (var i=0; i < word.length; i++){
+  answerarray[i] = "_";
+  spaces = document.getElementById('wordspaces');
+  spaces.innerHTML = answerarray.join(" ");
+}
+};
+
+function compare () {
+for (var h=0; h < word.length; h++){
+  if (word[h] === userguess){
+    answerarray[h] = (userguess);
+    spaces.innerHTML = answerarray.join(" ").toUpperCase();
+
+      }
+    }
+};
+
+function alphabetdisplay (){
+for (var j=0; j < alphabet.length; j++){
+  if (alphabet[j] === userguess){
+    alphabet[j] = ("__");
+    document.getElementById('Letters').innerHTML = alphabet.join(" ").toUpperCase();
+
+  };// ends Alphabet if statement
+}; //ends Alphabet for loop
+};
+
+function start (){
+  remainingguesses = 10;
+  if (remainingguesses==0){
+    document.getElementById('gameover').innerHTML="Sorry, you have run out of guesses. Your word was " + word +". Please press any key to start again!";
+    losses++;
+    return false;
+
+  }
+   else if (word===answerarray){
+      document.getElementById('gameover').innerHTML="Congratulations! You guessed correctly, your word was " + word +". Please press any key to start again!";
+      wins++;
+      return false;
+  } else {
+
+    document.getElementById('gameStart').innerHTML= "Try to guess the word! You have " + remainingguesses + " guesses remaining!";
+    document.getElementById('Letters').innerHTML = alphabet.join(" ").toUpperCase();
+
+    return true;
+  }
+};
+
+function picture (){
+  word.toString();
+  picture=0;
+  if (word == "gandalf"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/gandalf.jpg\">";
+  }
+  if (word == "valinor"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/valinor.jpg\">";
+  }
+  if (word == "sauron"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/sauron.jpg\">";
+  }
+  if (word == "galadriel"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/galadriel.jpg\">";
+  }
+  if (word == "balrog"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/balrog.jpg\">";
+  }
+  if (word == "gondor"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/gondor.png\">";
+  }
+  if (word == "rohan"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/rohan.jpg\">";
+  }
+  if (word == "moria"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/moria.jpg\">";
+  }
+  if (word == "shire"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/shire.jpg\">";
+  }
+  if (word == "baggins"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/baggins.jpg\">";
+  }
+  if (word == "gollum"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/gollum.jpg\">";
+  }
+  if (word == "frodo"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/frodo.jpg\">";
+  }
+  if (word == "orc"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/orc.jpg\">";
+  }
+  if (word == "troll"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/troll.jpg\">";
+  }
+  if (word == "dwarf"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/dwarf.jpg\">";
+  }
+  if (word == "fellowship"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/fellowship.jpg\">";
+
+  }
+  if (word == "aragorn"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/aragorn.jpg\">";
+
+  }
+  if (word == "boromir"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/boromir.jpg\">";
+
+  }
+  if (word == "legolas"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/legolas.jpg\">";
+  }
+  if (word == "merry"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/merry.jpg\">";
+
+  }
+  if (word == "pippin"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/pippin.jpg\">";
+
+  }
+  if (word == "sam"){
+    picture=document.getElementById('pic')
+    picture.innerHTML= "<img src=\"../images/sam.jpg\">";
+
+  }
+}
+
+
+
+startvar= start();
+if (startvar=true){
+    chooseword();
+  };
+
+
+console.log(word);
+
+document.onkeyup = function (event){
+  userguess=event.key;
+  console.log(userguess);
+}
+
+
+
+
+
+
+//       if (startvar==false){
+//
+//       }
+//
+// };
+//
+// };
---
+++ assets/javascript/placeholder.js
@@ -0,0 +1,92 @@
+
+var Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log(Alphabet.length);
+
+var SecretWords = ["Gandalf", "Valinor", "Sauron", "Galadriel", "Balrog", "Gondor", "Rohan", "Moria", "Shire", "Baggins", "Gollum", "Frodo", "Orc", "Troll", "Dwarf", "Fellowship", "Aragorn", "Boromir", "Legolas",
+"Merry", "Pippin", "Sam"];
+console.log(SecretWords.length);
+
+var Word=0;
+var Userguess;
+
+
+
+
+
+
+function play (){
+  Word = SecretWords[Math.floor(Math.random() * SecretWords.length)].toLowerCase();
+  Word = Word.split("");
+  document.getElementById('Letters').innerHTML=Alphabet.join(" ").toUpperCase();
+}
+
+
+
+
+  document.onkeyup = function(event){
+    if (Word===0){ //pick only one word to start the game
+    play();
+
+    var lives= 10;
+    document.getElementById('gameStart').innerHTML="Try to guess the word!";
+
+    var answerArray = [];
+    for (var i=0; i < Word.length; i++){
+      answerArray[i] = "_";
+    }
+
+
+
+    if (Word != answerArray) {
+      var spaces = document.getElementById('wordspaces');
+      spaces.innerHTML = answerArray.join(" ");
+
+
+
+      document.onkeyup = function  (event){
+        Userguess=event.key; //log user's letter guess
+
+        // console.log(Userguess);
+        for (var h=0; h < Word.length; h++){
+          if (Word[h] === Userguess){
+            answerArray[h] = (Userguess);
+            spaces.innerHTML = answerArray.join(" ").toUpperCase();
+            return answerArray;
+              }
+            };
+
+
+
+
+            for (var j=0; j < Alphabet.length; j++){
+              if (Alphabet[j] === Userguess){
+                Alphabet[j] = ("__");
+                document.getElementById('Letters').innerHTML = Alphabet.join(" ").toUpperCase();
+
+              };// ends Alphabet if statement
+            }; //ends Alphabet for loop
+
+
+
+
+        } //ends on key up
+        console.log (Userguess);
+        console.log (Word);
+
+        console.log (answerArray);
+
+
+        }     if(Word === answerArray)  {
+           // why doesn't this work? This SHOULD compare Word and answerArray, and if they contain the same values the code should run. Console logging both shows them containing the same values, however the code below does not run.
+           document.getElementById('gameStart').innerHTML= "Word guessed!";
+           document.getElementById('gameover').innerHTML= "Congratulations! You are correct. Your word was " + Word + ". Please press any key to start again";
+           Word = (0);
+          // console.log (Word);
+        };
+
+  }; //ends the original if statement setting Word to 0
+
+    // else{
+    //   //use for reset(?)
+    // }
+  };//ends the on key up function that starts the whole game
